dfa02f6e97c3f068032d68a83896082f957b41a1
mczerniawski <mczerniawski@users.noreply.github.com>
2017-03-12 19:25:24 +0100

    Added Verbosity and synopsis fix
    Added ConvertTo-HashtableFromPsCustomObject

--- ArcAdminTools/ArcAdminTools.psd1
+++ ArcAdminTools/ArcAdminTools.psd1
@@ -70,7 +70,8 @@ FormatsToProcess = 'ArcAdminTools.Format.ps1xml'
 # NestedModules = @()
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-FunctionsToExport = @('ConvertTo-HashtableFromJSON'
+FunctionsToExport = @('ConvertTo-HashtableFromJSON',
+		      'ConvertTo-HashtableFromPsCustomObject',
 		      'Add-ToLocalGroup'
 )
 
--- ArcAdminTools/Public/ConvertTo-HashtableFromJSON.ps1
+++ ArcAdminTools/Public/ConvertTo-HashtableFromJSON.ps1
@@ -1,23 +1,24 @@
 ﻿function ConvertTo-HashtableFromJSON {
+  #requires -Version 3.0 
   <#
-    .SYNOPSIS
-    Retrievies json file from disk and converts to hashtable.
+      .SYNOPSIS
+      Retrievies json file from disk and converts to hashtable.
 
-    .DESCRIPTION
-    Reads file given as Path parameter and using ConvertTo-HashtableFromPsCustomObject converts it to a hashtable.
+      .DESCRIPTION
+      Reads file given as Path parameter and using ConvertTo-HashtableFromPsCustomObject converts it to a hashtable.
 
-    .PARAMETER Path
-    Path to a json file.
+      .PARAMETER Path
+      Path to a json file.
 
-    .EXAMPLE
-    ConvertTo-HashtableFromJSON -Path c:\AdminTools\somefile.json
-    Will read somefile.json and convert it to custom hashtable.
+      .EXAMPLE
+      ConvertTo-HashtableFromJSON -Path c:\AdminTools\somefile.json
+      Will read somefile.json and convert it to custom hashtable.
 
-    .INPUTS
-    Path to a json file (string).
+      .INPUTS
+      Path to a json file (string).
 
-    .OUTPUTS
-    Custom Hashtable.
+      .OUTPUTS
+      Custom Hashtable.
   #>
 
 
@@ -30,19 +31,36 @@
              Mandatory = $true,   
              ValueFromPipeline = $true,  
              ValueFromPipelineByPropertyName = $true)]
-         [ValidateScript({Test-Path $_ -PathType 'Leaf' })]
+         [ValidateScript({Test-Path -Path $_ -PathType 'Leaf' })]
          
          [string]
          $Path 
      ) 
+  Begin {
+    Write-Verbose -Message "Starting $($MyInvocation.MyCommand) " 
+    Write-Verbose -Message 'Execution Metadata:'
+    Write-Verbose -Message "User = $($env:userdomain)\$($env:USERNAME)" 
+    Write-Verbose -Message "Computername = $env:COMPUTERNAME" 
+    Write-Verbose -Message "Host = $($host.Name)"
+    Write-Verbose -Message "PSVersion = $($PSVersionTable.PSVersion)"
+    Write-Verbose -Message "Runtime = $(Get-Date)" 
 
-process{
-    $content = Get-Content -LiteralPath $path -ReadCount 0 -Raw | Out-String
-    $pscustomObject = ConvertFrom-Json -InputObject $content
-    $hashtable = ConvertTo-HashtableFromPsCustomObject -psCustomObject $pscustomObject
+    Write-Verbose -Message "[$((get-date).TimeOfDay.ToString()) BEGIN   ] Starting: $($MyInvocation.Mycommand)"
     
-    return $hashtable;
-}
+  }
+  Process{
+    Write-Verbose -Message "[$((get-date).TimeOfDay.ToString()) PROCESS ] Processing json file {$Path}"
+    $content = Get-Content -LiteralPath $path -ReadCount 0 -Raw | Out-String
+    Write-Verbose -Message "[$((get-date).TimeOfDay.ToString()) PROCESS ] File read. Converting to PSCustomObject"
+    $pscustomObject = ConvertFrom-Json -InputObject $content
+    Write-Verbose -Message "[$((get-date).TimeOfDay.ToString()) PROCESS ] Converting from PSCustomObject to HashTable"
+    $hashtable = ConvertTo-HashtableFromPsCustomObject -psCustomObject $pscustomObject
+    $hashtable
+  }
+  End {
+    Write-Verbose -Message "[$((get-date).TimeOfDay.ToString()) END     ] Ending: $($MyInvocation.Mycommand)"
+    Write-Verbose -Message "Ending $($MyInvocation.MyCommand) " 
+  }
 }
 
 
